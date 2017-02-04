ad8ece83c047b31e8197d5f8e68e8a99e2b8e207
Josh Bowden <jbowden@hawk.iit.edu>
2017-02-03 22:20:31 -0600

    feat(mp02): implement hashtable lab except test06 has wrong size bug

--- mps/02/hashtable.c
+++ mps/02/hashtable.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "hashtable.h"
 
 /* Daniel J. Bernstein's "times 33" string hash function, from comp.lang.C;
@@ -21,8 +22,25 @@ hashtable_t *make_hashtable(unsigned long size) {
   return ht;
 }
 
+static bucket_t *ht_get_node(hashtable_t *ht, char *key) {
+  unsigned int idx = hash(key) % ht->size;
+  bucket_t *b = ht->buckets[idx];
+  while (b) {
+    if (strcmp(b->key, key) == 0) {
+      return b;
+    }
+    b = b->next;
+  }
+  return NULL;
+}
+
 void ht_put(hashtable_t *ht, char *key, void *val) {
-  /* FIXME: the current implementation doesn't update existing entries */
+  bucket_t *existing = ht_get_node(ht, key);
+  if (existing) {
+    existing->val = val;
+    return;
+  }
+
   unsigned int idx = hash(key) % ht->size;
   bucket_t *b = malloc(sizeof(bucket_t));
   b->key = key;
@@ -32,38 +50,101 @@ void ht_put(hashtable_t *ht, char *key, void *val) {
 }
 
 void *ht_get(hashtable_t *ht, char *key) {
-  unsigned int idx = hash(key) % ht->size;
-  bucket_t *b = ht->buckets[idx];
-  while (b) {
-    if (strcmp(b->key, key) == 0) {
-      return b->val;
+  bucket_t *node = ht_get_node(ht, key);
+  if (!node) return NULL;
+  return node->val;
+}
+
+static void ht_iter_bucket(hashtable_t *ht, int (*f)(bucket_t *, void *), void *ctx) {
+  bucket_t *next;
+  bucket_t *b;
+  unsigned long i;
+  for (i = 0; i < ht->size; i++) {
+    b = ht->buckets[i];
+    while (b) {
+      next = b->next;
+      if (!f(b, ctx)) {
+        return ; // abort iteration
+      }
+      b = next;
     }
-    b = b->next;
   }
-  return NULL;
+}
+
+static int ht_iter_f(bucket_t *b, void *ctx) {
+  int (*f)(char *, void *) = ctx;
+  f(b->key, b->val);
+  return true;
 }
 
 void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
-  bucket_t *b;
-  unsigned long i;
-  for (i=0; i<ht->size; i++) {
-    b = ht->buckets[i];
-    while (b) {
-      if (!f(b->key, b->val)) {
-        return ; // abort iteration
-      }
-      b = b->next;
-    }
-  }
+  ht_iter_bucket(ht, ht_iter_f, f);
+}
+
+static void free_bucket(bucket_t *b) {
+  free(b->key);
+  free(b->val);
+  free(b);
+}
+
+static int free_bucket_f(bucket_t *b, void *ignored) {
+  free_bucket(b);
+  return true;
 }
 
 void free_hashtable(hashtable_t *ht) {
-  free(ht); // FIXME: must free all substructures!
+  ht_iter_bucket(ht, free_bucket_f, NULL);
+  free(ht);
 }
 
-/* TODO */
-void  ht_del(hashtable_t *ht, char *key) {
+void ht_del(hashtable_t *ht, char *key) {
+  unsigned int idx = hash(key) % ht->size;
+  bucket_t *root = ht->buckets[idx];
+  bucket_t *cur = root;
+  bucket_t *prev = NULL;
+
+  while (cur) {
+    if (strcmp(key, cur->key) == 0) {
+      if (!prev) {
+        ht->buckets[idx] = NULL;
+        free_bucket(root);
+      } else {
+        prev->next = cur->next;
+        free_bucket(cur);
+      }
+      return;
+    }
+    prev = cur;
+    cur = cur->next;
+  }
 }
 
-void  ht_rehash(hashtable_t *ht, unsigned long newsize) {
+static int reinsert_f(bucket_t *b, void *ctx) {
+  hashtable_t *new = ctx;
+  ht_put(new, b->key, b->val);
+  return true;
+}
+
+void ht_rehash(hashtable_t *ht, unsigned long newsize) {
+  hashtable_t *ht_new = make_hashtable(newsize);
+  ht_iter_bucket(ht, reinsert_f, ht_new);
+
+  // free the old bucket_t containers
+  bucket_t *tmp;
+  bucket_t *cur;
+  unsigned long i;
+  for (i = 0; i < ht->size; i++) {
+    cur = ht->buckets[i];
+    while (cur) {
+      tmp = cur->next;
+      free(cur);
+      cur = tmp;
+    }
+    ht->buckets[i] = NULL;
+  }
+
+  ht->size = ht_new->size;
+  ht->buckets = ht_new->buckets;
+
+  free(ht_new); // only free the temp container
 }
